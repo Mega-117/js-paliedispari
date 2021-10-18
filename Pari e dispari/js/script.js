fe31c8ead9ce5e8152a484009ec97c489d742e7a
Mega-117 <enricosaian@hotmail.it>
2021-10-18 21:18:34 +0200

    revisione

--- Pari e dispari/js/script.js
+++ Pari e dispari/js/script.js
@@ -3,55 +3,44 @@ console.log(bntPari);
 const bntDispari = document.getElementById("btn-dispari");
 console.log(bntDispari);
 
-function controlloPariDispari(numero1) {
+function controlloPariDispari(numero1,) {
 
     let numeroPc = parseInt(Math.ceil(Math.random() * 5));
     console.log("numero pc " + numeroPc);
 
     let somma = numero1 + numeroPc;
-    const sommaNumeri = somma;
-    console.log(sommaNumeri);
+    console.log(somma);
     if (somma % 2 === 0) {
-        somma = "pari";
-        return somma;
-
+        return true;
     } else {
-        somma = "dispari";
-        return somma;
+        return false;
+    }
+}
 
+function gioco(scelta) {
+    const numeroUtente = parseInt(prompt("insetisci un numero tra 1 e 5"));
+    console.log("numero utente " + numeroUtente);
+    let controlloNumero = controlloPariDispari(numeroUtente);
+    //se il btn è pari e la somma finale è pari ho vinto
+
+    if (scelta === "pari" && controlloNumero) {
+        alert("il bottone premuto è pari la somma è pari hai vinto");
+    }
+    //se il btn è dispari e la somma è dispari ho vinto
+    else if (scelta === "dispari" && !controlloNumero) {
+        alert("hai vinto perche dispari");
+    }
+    //altrimenti ho perso
+    else {
+        alert("hai perso");
     }
 }
 
 bntPari.addEventListener("click", function () {
-    const numeroUtente = parseInt(prompt("insetisci un numero tra 1 e 5"));
-    console.log("numero utente " + numeroUtente);
 
-    let controlloNumero = controlloPariDispari(numeroUtente);
-
-    console.log(controlloNumero);
-    if (controlloNumero === "pari") {
-        alert("hai vinto");
-        console.log(controlloNumero);
-    } else {
-        console.log(controlloNumero);
-        alert("hai perso");
-    }
-
+    gioco("pari");
 });
 
 bntDispari.addEventListener("click", function () {
-    const numeroUtente = parseInt(prompt("insetisci un numero tra 1 e 5"));
-    console.log("numero utente " + numeroUtente);
-
-    let controlloNumero = controlloPariDispari(numeroUtente);
-
-    console.log(controlloNumero);
-    if (controlloNumero === "dispari") {
-        alert("hai vinto");
-        console.log(controlloNumero);
-    } else {
-        console.log(controlloNumero);
-        alert("hai perso");
-    }
-
+    gioco("dispari");
 });
